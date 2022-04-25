cbbbe006dc9da710ca5c6ed56bdf4163410d33ea
Md Naushad Ahamed <99961853+naushadcom@users.noreply.github.com>
2022-04-25 09:26:57 +0530

    file uploaded

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Main from './Todo/Main';
+
+
+
+
+// import Employ from './sprint-1/day-3/assignments/PaymentCard/Employ';
+// import Todo from './sprint-1/day-3/assignments/Todo/Todo';
+
+
+
+
+
+
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+     {/* <ShowHide/> */}
+
+    <Main/>
+
+
+    </div>
+  );
+}
+
+export default App;
